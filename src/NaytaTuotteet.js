eb45ae76c8335792bf3dc1d1445ede044aa2333a
mikokallio@gmail.com <miko@localhost.localdomain>
2022-09-01 13:07:26 +0300

    shows products of one company

--- src/NaytaTuotteet.js
+++ src/NaytaTuotteet.js
@@ -2,23 +2,39 @@ import './App.css';
 import { gql, useQuery } from '@apollo/client';
 import {useContext} from "react";
 import {SigninContext} from "./App";
+import {useAuth0} from "@auth0/auth0-react";
 
 function NaytaTuotteet() {
     const { userName, setUserName, setDisplayProfile } = useContext(SigninContext)
+    const { user } = useAuth0();
+
 
     const GET_TUOTTEET = gql`
-query MyQuery {
-  tyokone {
+query MyQuery($yritys: String!) {
+  tyokone(where: {yritys: {_eq: $yritys}}) {
     id
     lisatietoja
     malli
     vapaateksti
     yritys
+    voltit
+    tyyppi
+    teho
+    paino
+    ostopvm
+    merkki
+    hertz
+    ce
+    ampeeri
   }
 }
 `;
 
-    const { loading, error, data } = useQuery(GET_TUOTTEET);
+    const yritys = user.email;
+
+    const { loading, error, data } = useQuery(GET_TUOTTEET, {
+        variables: { yritys },
+    });
 
     const naytaTuote = (tyokone) => {
         setUserName("naytatuote")
@@ -32,7 +48,8 @@ query MyQuery {
              <div className={"App-kortti"} onClick={event => naytaTuote(tyokone.id)}>
                  <h3>Sarjanumero: {tyokone.id}</h3>
                  <h3>Malli: {tyokone.malli}</h3>
-                 <h3>Yritys: {tyokone.yritys}</h3>
+                 <h3>Merkki: {tyokone.merkki}</h3>
+                 <h3>Omistaja: {tyokone.yritys}</h3>
              </div>
              </li>);
     }
